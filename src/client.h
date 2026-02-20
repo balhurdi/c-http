1f80baf9b5898cac55f7b98f6709e1d744a2fbf8
Francisco Torrinha <francisco.torrinha@gmail.com>
2026-02-20 21:22:37 +0000

    feat: Added dead client interface
    This is a blueprint for how the client interface will look in the future

---
+++ src/client.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct _client *client_t;
+typedef struct _client_pool *client_pool_t;
+
+client_pool_t client_pool_new();
+void client_pool_add_client(client_pool_t pool, int32_t client_fd);
+void client_pool_remove_client(client_pool_t pool, int32_t client_fd);
+client_t client_pool_get_client(client_pool_t pool, int32_t client_fd);
+void client_pool_drop(client_pool_t client_pool);
+
+void client_recvd_data(client_t client, char* data, size_t *size);
+void client_send_data(client_t client, const char *data, size_t data_len);
