20e45945d506938159c02c34f002ffde2eebf3ca
Francisco Torrinha <francisco.torrinha@gmail.com>
2026-02-27 16:18:20 +0000

    refactor: Seperated lib steps in funcs

--- mate.c
+++ mate.c
@@ -1,26 +1,33 @@
 #define MATE_IMPLEMENTATION
 #include "mate.h/mate.h"
 
+void build_server_lib() {
+  StaticLib server_lib =
+      CreateStaticLib((StaticLibOptions){.output = "libserver",
+                                         .debug = FLAG_DEBUG,
+                                         .warnings = FLAG_WARNINGS_VERBOSE});
+
+  AddFile(server_lib, "./src/server.c");
+  InstallStaticLib(server_lib);
+}
+
+void build_server_exec() {
+  Executable executable =
+      CreateExecutable((ExecutableOptions){.output = "main",
+                                           .debug = FLAG_DEBUG,
+                                           .warnings = FLAG_WARNINGS_VERBOSE});
+  AddFile(executable, "./src/main.c");
+  AddLibraryPaths(executable, "./build");
+  LinkSystemLibraries(executable, "server");
+
+  InstallExecutable(executable);
+}
+
 i32 main(void) {
   StartBuild();
   {
-    StaticLib server_lib =
-        CreateStaticLib((StaticLibOptions){.output = "libserver",
-                                           .debug = FLAG_DEBUG,
-                                           .warnings = FLAG_WARNINGS_VERBOSE});
-
-    AddFile(server_lib, "./src/server.c");
-    InstallStaticLib(server_lib);
-
-    Executable executable =
-        CreateExecutable((ExecutableOptions){.output = "main",
-                                             .debug = FLAG_DEBUG,
-                                             .warnings = FLAG_WARNINGS_VERBOSE});
-    AddFile(executable, "./src/main.c");
-    AddLibraryPaths(executable, "./build");
-    LinkSystemLibraries(executable, "server");
-
-    InstallExecutable(executable);
+    build_server_lib();
+    build_server_exec();
   }
   EndBuild();
 }
